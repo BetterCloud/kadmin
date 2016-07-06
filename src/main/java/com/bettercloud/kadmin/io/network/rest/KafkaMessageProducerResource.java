60af0486722cfe4cd9ec95e87d2b1e04a5a6c236
David Esposito <david.esposito@bettercloud.com>
2016-07-06 05:39:32 -0400

    Adding ability to iterate sending messages for load testing.

--- src/main/java/com/bettercloud/kadmin/io/network/rest/KafkaMessageProducerResource.java
+++ src/main/java/com/bettercloud/kadmin/io/network/rest/KafkaMessageProducerResource.java
@@ -3,19 +3,21 @@ package com.bettercloud.kadmin.io.network.rest;
 import com.bettercloud.kadmin.api.kafka.KafkaMessageConverter;
 import com.bettercloud.kadmin.api.kafka.KafkaProviderService;
 import com.bettercloud.kadmin.api.models.KafkaProduceMessageMeta;
+import com.bettercloud.logger.services.LogLevel;
+import com.bettercloud.logger.services.Logger;
+import com.bettercloud.logger.services.LoggerFactory;
 import com.bettercloud.messaging.kafka.produce.ProducerService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.JsonNodeType;
+import lombok.*;
 import org.apache.avro.generic.GenericRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * Created by davidesposito on 7/1/16.
@@ -24,6 +26,7 @@ import java.io.IOException;
 @RequestMapping("/kafka/send")
 public class KafkaMessageProducerResource {
 
+    private static final Logger logger = LoggerFactory.getLogger(KafkaMessageProducerResource.class);
     private static final ObjectMapper mapper = new ObjectMapper();
 
     private final KafkaProviderService providerService;
@@ -40,7 +43,8 @@ public class KafkaMessageProducerResource {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public ResponseEntity<Object> send(@RequestBody KafkaProduceRequestModel requestModel) {
+    public ResponseEntity<ProducerResponse> send(@RequestBody KafkaProduceRequestModel requestModel,
+                                       @RequestParam("count") Optional<Integer> oCount) {
         if (requestModel.getRawMessage().getNodeType().equals(JsonNodeType.STRING)) {
             try {
                 requestModel.setRawMessage(mapper.readTree(requestModel.getRawMessage().asText()));
@@ -52,9 +56,44 @@ public class KafkaMessageProducerResource {
         GenericRecord message = converter.convert(requestModel.getRawMessage(), meta);
         ProducerService<String, Object> ps = providerService.producerService(meta.getKafkaUrl(), meta.getSchemaRegistryUrl());
         boolean sendMessage = message != null && ps != null;
+        int count = 0;
+        boolean success = false;
+        long duration = -1;
+        double rate = -1;
         if (sendMessage) {
-            ps.send(meta.getTopic(), message);
+            count = oCount.orElse(1);
+            success = true;
+            long startTime = System.currentTimeMillis();
+            for (int i=0;i<count;i++) {
+                try {
+                    ps.send(meta.getTopic(), message);
+                } catch (Exception e) {
+                    success = false;
+                }
+            }
+            duration = System.currentTimeMillis() - startTime;
+            rate = count * 1000.0 / duration;
         }
-        return ResponseEntity.ok(sendMessage);
+        ProducerResponse res = ProducerResponse.builder()
+                .count(count)
+                .duration(duration)
+                .rate(rate)
+                .sent(sendMessage)
+                .success(success)
+                .build();
+        logger.log(LogLevel.INFO, "Produced: {}", res);
+        return ResponseEntity.ok(res);
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ProducerResponse {
+        private int count;
+        private long duration;
+        private double rate;
+        private boolean success;
+        private boolean sent;
     }
 }
